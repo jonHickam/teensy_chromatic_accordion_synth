eb5810b439830a44175f6aadcb0a865a270369f1
jonHickam <jon.hickam@gmail.com>
2019-02-05 20:10:25 -0700

    split out joystick read

--- 36-joystick-vol-envelope-pitch/36-joystick-vol-envelope-pitch.ino
+++ 36-joystick-vol-envelope-pitch/36-joystick-vol-envelope-pitch.ino
@@ -241,53 +241,7 @@ void loop() {
   LastButtonA = ButtonA;
   LastButtonB = ButtonB;
   // read the joysticks
-    if (JoystickReadMillis >= 50) {
-    JoystickReadMillis = 0;
-      
-    float latestRead = analogRead(JoyXPin);
-    if (abs(latestRead - xAxis) > 10){
-      frequencyMod = map(latestRead,1,1024,1056,944);
-      xAxis = latestRead;
-   //   usbMIDI.sendControlChange(xAxisController, xAxisPin, channel);
-
-        if (pitchGlobal == 0) {                             // just bend the last note played
-
-          if (nextWaveform == 1 && waveformNum[0] > 0  )
-            {
-            waveform[0].frequency(Frequency[waveformNum[0]]* frequencyMod /1000) ;
-            }
-          else if (nextWaveform == 2 && waveformNum[1] > 0  )
-            {
-            waveform[1].frequency(Frequency[waveformNum[1]]* frequencyMod /1000) ;
-            }
-          else if (nextWaveform == 3 && waveformNum[2] > 0  )
-            {
-            waveform[2].frequency(Frequency[waveformNum[2]]* frequencyMod /1000) ;
-            }
-          else if (nextWaveform == 0 && waveformNum[3] > 0  )
-            {
-            waveform[3].frequency(Frequency[waveformNum[3]]* frequencyMod /1000) ;
-            }
-        }
-        else if (pitchGlobal == 1) {                      // bend all of the notes
-          for (int x = 0; x< 4; x++){
-            waveform[x].frequency(Frequency[waveformNum[x]]* frequencyMod /1000) ;
-          }
-        }
- 
-    } //xAxis read
-    
-    latestRead = analogRead(JoyYPin);
-    if (abs(latestRead - yAxis) > 10){
-      float yMapped = map(latestRead,1,1024,2.0,0.0);
-      amp1.gain(yMapped);
-   //   usbMIDI.sendControlChange(yAxisController, yAxisPin, channel);
-      yAxis = latestRead;
-    }  //yAxis read
-    
-   } // msec timer   
-  
-  
+  readJoystick() ;
   // read the array
   for(int rInt = 0; rInt < rSize; rInt ++) {
    
